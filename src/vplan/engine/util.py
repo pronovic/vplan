961bca69044fb0608968438c13de9d3671bc6a42
Kenneth J. Pronovici <pronovic@ieee.org>
2021-11-07 22:27:09 -0600

    Improve the database directory check

--- src/vplan/engine/util.py
+++ src/vplan/engine/util.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # vim: set ft=python ts=4 sw=4 expandtab:
 from os import makedirs, stat
+from stat import S_IMODE
 
 from vplan.engine.config import config
 from vplan.engine.interface import ServerException
@@ -9,8 +10,9 @@ from vplan.engine.interface import ServerException
 def _create_and_check_dir(name: str, path: str) -> None:
     """Create a directory if needed, and check its permissions."""
     makedirs(path, mode=0o700, exist_ok=True)
-    if stat(path).st_mode != 0o700:
-        raise ServerException("%s must have permissions 700 (drwx------): %s" % (name, path))
+    mode = S_IMODE(stat(path).st_mode)
+    if mode != 0o700:
+        raise ServerException("%s must have permissions 700, but has %o: %s" % (name, mode, path))
 
 
 def setup_directories() -> None:
