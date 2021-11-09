024cd731d3805755f72d31c6e3bf5a34bb232cdd
Kenneth J. Pronovici <pronovic@ieee.org>
2021-11-08 21:44:01 -0600

    Fix errors in API interface

--- src/vplan/engine/routers/account.py
+++ src/vplan/engine/routers/account.py
@@ -22,13 +22,13 @@ def retrieve_account() -> Account:
 
 
 @ROUTER.post("/account", status_code=HTTP_201_CREATED, response_class=EmptyResponse)
-def create_account() -> None:
+def create_account(account: Account) -> None:
     """Create your account in the plan engine."""
     return None
 
 
 @ROUTER.put("/account", status_code=HTTP_204_NO_CONTENT, response_class=EmptyResponse)
-def update_account() -> None:
+def update_account(account: Account) -> None:
     """Update your account in the plan engine."""
     return None
 
--- src/vplan/engine/routers/plan.py
+++ src/vplan/engine/routers/plan.py
@@ -11,7 +11,7 @@ from fastapi import APIRouter
 from starlette.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_204_NO_CONTENT
 
 from vplan.engine.fastapi.extensions import EmptyResponse
-from vplan.engine.interface import Device, DeviceGroup, PlanSchema, Status
+from vplan.engine.interface import PlanSchema, Status
 
 ROUTER = APIRouter()
 
@@ -23,7 +23,7 @@ def retrieve_all_plans() -> List[str]:
 
 
 @ROUTER.get("/plan/{plan_name}", status_code=HTTP_200_OK)
-def retrieve_plan() -> PlanSchema:
+def retrieve_plan(plan_name: str) -> PlanSchema:
     """Return the plan definition stored in the plan engine."""
     return None  # type: ignore
 
@@ -34,7 +34,7 @@ def create_plan(plan: PlanSchema) -> None:
 
 
 @ROUTER.put("/plan/{plan_name}", status_code=HTTP_204_NO_CONTENT, response_class=EmptyResponse)
-def update_plan(plan: PlanSchema) -> None:
+def update_plan(plan_name: str, plan: PlanSchema) -> None:
     """Update an existing plan in the plan engine."""
 
 
@@ -44,20 +44,25 @@ def delete_plan(plan_name: str) -> None:
 
 
 @ROUTER.get("/plan/{plan_name}/status", status_code=HTTP_200_OK)
-def retrieve_status() -> Status:
+def retrieve_status(plan_name: str) -> Status:
     """Return the enabled/disabled status of a plan in the plan engine."""
 
 
 @ROUTER.put("/plan/{plan_name}/status", status_code=HTTP_204_NO_CONTENT, response_class=EmptyResponse)
-def update_status(status: Status) -> None:
+def update_status(plan_name: str, status: Status) -> None:
     """Set the enabled/disabled status of a plan in the plan engine."""
 
 
-@ROUTER.post("/plan/{plan_name}/test/group", status_code=HTTP_204_NO_CONTENT, response_class=EmptyResponse)
-def test_group(group: DeviceGroup, toggle_count: Optional[int]) -> None:
+@ROUTER.post("/plan/{plan_name}/refresh", status_code=HTTP_204_NO_CONTENT, response_class=EmptyResponse)
+def refresh_plan(plan_name: str) -> None:
+    """Refresh the plan rules in the SmartThings infrastructure."""
+
+
+@ROUTER.post("/plan/{plan_name}/test/group/{group_name}", status_code=HTTP_204_NO_CONTENT, response_class=EmptyResponse)
+def test_group(plan_name: str, group_name: str, toggle_count: Optional[int]) -> None:
     """Test a device group that is part of a plan."""
 
 
-@ROUTER.post("/plan/{plan_name}/test/device", status_code=HTTP_204_NO_CONTENT, response_class=EmptyResponse)
-def test_device(device: Device, toggle_count: Optional[int]) -> None:
+@ROUTER.post("/plan/{plan_name}/test/device/{room}/{device}", status_code=HTTP_204_NO_CONTENT, response_class=EmptyResponse)
+def test_device(plan_name: str, room: str, device: str, toggle_count: Optional[int]) -> None:
     """Test a device that is part of a plan."""
