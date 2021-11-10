331cc5bec58b354df755b6a8ef896b9fbc178b79
Kenneth J. Pronovici <pronovic@ieee.org>
2021-11-09 20:24:04 -0600

    Simplify error handling:

--- src/vplan/client/client.py
+++ src/vplan/client/client.py
@@ -6,8 +6,10 @@ API client written in terms of Python requests.
 """
 from typing import List, Optional
 
+import click
 import requests
 import requests_unixsocket
+from requests import HTTPError, Response
 
 from vplan.client.config import api_url
 from vplan.engine.interface import Account, PlanSchema, Status
@@ -31,36 +33,43 @@ def _plan(endpoint: str = "") -> str:
     return _url("/plan%s" % endpoint)
 
 
+def _raise_for_status(response: Response) -> None:
+    """Check response status, raising ClickException for errors"""
+    try:
+        response.raise_for_status()
+    except HTTPError as e:
+        raise click.ClickException("%s" % e) from e
+
+
 def retrieve_account() -> Optional[Account]:
     """Retrieve account information stored in the plan engine."""
     url = _account()
     response = requests.get(url=url)
     if response.status_code == 404:
         return None
-    else:
-        response.raise_for_status()
-        return Account.parse_raw(response.json())
+    _raise_for_status(response)
+    return Account.parse_raw(response.json())
 
 
 def create_account(account: Account) -> None:
     """Create your account in the plan engine."""
     url = _account()
     response = requests.post(url=url, json=account.json())
-    response.raise_for_status()
+    _raise_for_status(response)
 
 
 def update_account(account: Account) -> None:
     """Update your account in the plan engine."""
     url = _account()
     response = requests.put(url=url, json=account.json())
-    response.raise_for_status()
+    _raise_for_status(response)
 
 
 def delete_account() -> None:
     """Delete account information stored in the plan engine."""
     url = _account()
     response = requests.delete(url=url)
-    response.raise_for_status()
+    _raise_for_status(response)
 
 
 def retrieve_account_status() -> Optional[Status]:
@@ -69,7 +78,7 @@ def retrieve_account_status() -> Optional[Status]:
     response = requests.get(url=url)
     if response.status_code == 404:
         return None
-    response.raise_for_status()
+    _raise_for_status(response)
     return Status.parse_raw(response.json())
 
 
@@ -77,14 +86,14 @@ def update_account_status(status: Status) -> None:
     """Set the enabled/disabled status of your account in the plan engine."""
     url = _account("/status")
     response = requests.put(url=url, json=status.json())
-    response.raise_for_status()
+    _raise_for_status(response)
 
 
 def retrieve_all_plans() -> List[str]:
     """Return the names of all plans stored in the plan engine."""
     url = _plan()
     response = requests.get(url=url)
-    response.raise_for_status()
+    _raise_for_status(response)
     return response.json()  # type: ignore
 
 
@@ -94,7 +103,7 @@ def retrieve_plan(plan_name: str) -> Optional[PlanSchema]:
     response = requests.get(url=url)
     if response.status_code == 404:
         return None
-    response.raise_for_status()
+    _raise_for_status(response)
     return PlanSchema.parse_raw(response.json())
 
 
@@ -102,21 +111,21 @@ def create_plan(plan: PlanSchema) -> None:
     """Create a plan in the plan engine."""
     url = _plan()
     response = requests.post(url=url, json=plan.json())
-    response.raise_for_status()
+    _raise_for_status(response)
 
 
 def update_plan(plan: PlanSchema) -> None:
     """Update an existing plan in the plan engine."""
     url = _plan("/%s" % plan.plan.name)
     response = requests.put(url=url, json=plan.json())
-    response.raise_for_status()
+    _raise_for_status(response)
 
 
 def delete_plan(plan_name: str) -> None:
     """Delete a plan stored in the plan engine."""
     url = _plan("/%s" % plan_name)
     response = requests.delete(url=url)
-    response.raise_for_status()
+    _raise_for_status(response)
 
 
 def retrieve_plan_status(plan_name: str) -> Optional[Status]:
@@ -125,7 +134,7 @@ def retrieve_plan_status(plan_name: str) -> Optional[Status]:
     response = requests.get(url=url)
     if response.status_code == 404:
         return None
-    response.raise_for_status()
+    _raise_for_status(response)
     return Status.parse_raw(response.json())
 
 
@@ -133,14 +142,14 @@ def update_plan_status(plan_name: str, status: Status) -> None:
     """Set the enabled/disabled status of a plan in the plan engine."""
     url = _plan("/%s/status" % plan_name)
     response = requests.put(url=url, json=status.json())
-    response.raise_for_status()
+    _raise_for_status(response)
 
 
 def refresh_plan(plan_name: str) -> None:
     """Refresh the plan rules in the SmartThings infrastructure."""
     url = _plan("/%s/refresh" % plan_name)
     response = requests.post(url=url)
-    response.raise_for_status()
+    _raise_for_status(response)
 
 
 def toggle_group(plan_name: str, group_name: str, toggle_count: int) -> None:
@@ -148,7 +157,7 @@ def toggle_group(plan_name: str, group_name: str, toggle_count: int) -> None:
     url = _plan("/%s/test/group/%s" % (plan_name, group_name))
     params = {"toggle_count": toggle_count}
     response = requests.post(url=url, params=params)
-    response.raise_for_status()
+    _raise_for_status(response)
 
 
 def toggle_device(plan_name: str, room: str, device: str, toggle_count: int) -> None:
@@ -156,4 +165,4 @@ def toggle_device(plan_name: str, room: str, device: str, toggle_count: int) -> 
     url = _plan("/%s/test/device/%s/%s" % (plan_name, room, device))
     params = {"toggle_count": toggle_count}
     response = requests.post(url=url, params=params)
-    response.raise_for_status()
+    _raise_for_status(response)
