2fd8adc88b84ad45d69343bf647e4cf8a4604020
Kenneth J. Pronovici <pronovic@ieee.org>
2021-11-14 09:32:48 -0600

    Don't use relative imports

--- src/vplan/engine/server.py
+++ src/vplan/engine/server.py
@@ -10,13 +10,13 @@ from fastapi import FastAPI, Request
 from sqlalchemy.exc import NoResultFound
 from starlette.responses import Response
 
-from .database import setup_database
-from .exception import AlreadyExistsError
-from .fastapi.extensions import EmptyResponse
-from .interface import Health, Version
-from .routers import account, plan
-from .scheduler import shutdown_scheduler, start_scheduler
-from .util import setup_directories
+from vplan.engine.database import setup_database
+from vplan.engine.exception import AlreadyExistsError
+from vplan.engine.fastapi.extensions import EmptyResponse
+from vplan.engine.interface import Health, Version
+from vplan.engine.routers import account, plan
+from vplan.engine.scheduler import shutdown_scheduler, start_scheduler
+from vplan.engine.util import setup_directories
 
 API_VERSION = "1.0.0"
 API = FastAPI(version=API_VERSION, docs_url=None, redoc_url=None)  # no Swagger or ReDoc endpoints
